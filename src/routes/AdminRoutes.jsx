eba8d28a621699afee021b3130095f14c3bb3256
Mohammed Sinan Mp <sinan.m.p333@gmail.com>
2024-08-29 21:23:45 +0530

    admin side main content paddin changed

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -12,7 +12,7 @@ function AdminRoutes() {
         <Sidebar/>
         <div className="flex-1 flex flex-col">
           <Navbar />
-          <div className="p-6 flex-1 overflow-y-auto">
+          <div className="p-2 md:p-6 flex-1 overflow-y-auto">
             <Routes>
               <Route path="/products" element={<List/>}/>
               <Route path="/dashboard" element={<Dashboard/>}/>
