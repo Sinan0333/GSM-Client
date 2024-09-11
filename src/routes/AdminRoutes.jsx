7aed7d67828574763b8e3b9112637deec562ebc6
sinan <sinan.m.p333@gmail.com>
2024-09-11 19:37:27 +0530

    pages added

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Sidebar from '../components/admin/Sidebar'
 import Navbar from '../components/admin/Navbar'
-import List from '../components/admin/List'
-import Dashboard from '../components/admin/Dashboard'
-import AddProduct from '../components/admin/AddProduct'
+import List from '../pages/admin/List'
+import Dashboard from '../pages/admin/Dashboard'
+import AddProduct from '../pages/admin/AddProduct'
 
 function AdminRoutes() {
   return (
@@ -12,7 +12,7 @@ function AdminRoutes() {
         <Sidebar/>
         <div className="flex-1 flex flex-col">
           <Navbar />
-          <div className="p-2 md:p-6 flex-1 overflow-y-auto">
+          <div className="p-2 md:p-4 flex-1 overflow-y-auto">
             <Routes>
               <Route path="/products" element={<List/>}/>
               <Route path="/dashboard" element={<Dashboard/>}/>
